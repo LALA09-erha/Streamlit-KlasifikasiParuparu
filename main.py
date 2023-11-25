4673d2ab8f7ccdc5adc24a53968c66ef6f44f12b
erha <fikri41153.pesonaxii@gmail.com>
2023-11-25 21:58:41 +0700

    update

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import time
 
 # pige title
 st.set_page_config(
-    page_title="Prediksi Penyakit Jantung",
-    page_icon="https://e7.pngegg.com/pngimages/594/747/png-clipart-heart-heart-cartoon-heart.png",
+    page_title="Klasifikasi Penyakit Paru-paru",
+    page_icon="https://png.pngtree.com/png-clipart/20190520/original/pngtree-red-lung-pink-lung-tube-cartoon-illustration-hand-drawn-organ-illustration-png-image_3874739.jpg",
 )
 
     # 0 = tidak ada penyakit jantung
@@ -26,8 +26,8 @@ header {visibility: hidden;}
 
 """
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">', unsafe_allow_html=True)
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-st.markdown(' <div style="position: fixed; top: 0; left: 0; z-index: 9999; width: 100%; background: rgb(14, 17, 23); ; text-align: center;"><a href="https://github.com/LALA09-erha/Streamlit---WebPrediksiKNN" target="_blank"><button style="border-radius: 12px;position: relative; top:50%; margin:10px;"><i class="fa fa-github"></i> Source Code</button></a><a href="https://lala09-erha.github.io/datamining/intro.html" target="_blank"><button  style="border-radius: 12px;position: relative; top:50%;"><i style="color: orange" class="fa fa-book"></i> Jupyter Book</button></a></div>', unsafe_allow_html=True)
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+# st.markdown(' <div style="position: fixed; top: 0; left: 0; z-index: 9999; width: 100%; background: rgb(14, 17, 23); ; text-align: center;"><a href="https://github.com/LALA09-erha/Streamlit---WebPrediksiKNN" target="_blank"><button style="border-radius: 12px;position: relative; top:50%; margin:10px;"><i class="fa fa-github"></i> Source Code</button></a><a href="https://lala09-erha.github.io/datamining/intro.html" target="_blank"><button  style="border-radius: 12px;position: relative; top:50%;"><i style="color: orange" class="fa fa-book"></i> Jupyter Book</button></a></div>', unsafe_allow_html=True)
 
 
 
@@ -39,142 +39,110 @@ about = kolom[2].button('About')
 
 # home page
 if home==False and about==False or home==True and about==False:
-    st.markdown("<h1 style='text-align: center; color: white; margin:0 ; padding:0;'>Prediksi Penyakit Jantung</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: white; margin:0 ; padding:0;'>Klasifikasi Penyakit Paru-paru</h1>", unsafe_allow_html=True)
     # setting kolom
-    with st.expander("Setting"):
+    # with st.expander("Setting"):
 
-        preprosesing = st.radio('Preprocessing Data', options=['Normalization (Min-Max)', 'Normal'], index=0, horizontal=True)
-        st.markdown("""---""")
-        st.write("""Metode Klasfikasi""")
+    #     preprosesing = st.radio('Preprocessing Data', options=['Normalization (Min-Max)', 'Normal'], index=0, horizontal=True)
+    #     st.markdown("""---""")
+    #     st.write("""Metode Klasfikasi""")
 
-        model_1 = st.checkbox('K-Nearest Neighbors (K=11) ', value=True)
-        model_2 = st.checkbox('Bagging Clasifier (Gaussian) ')
-        model_3 = st.checkbox('Random Forest')
+    #     model_1 = st.checkbox('K-Nearest Neighbors (K=11) ', value=True)
+    #     model_2 = st.checkbox('Bagging Clasifier (Gaussian) ')
+    #     model_3 = st.checkbox('Random Forest')
 
     col1, col2 = st.columns(2)
     with col1:
         nama = st.text_input("Masukkan Nama",placeholder='Nama')
     with col2:
         umur = st.number_input("Masukkan Umur",max_value=100)
-    jk = st.selectbox("Jenis Kelamin",('Laki-laki','Perempuan'))
+    col9,col10 = st.columns(2)
+    with col9:
+        jk = st.selectbox("Jenis Kelamin",('Laki-laki','Perempuan'))
+    with col10:
+        batuk = st.selectbox("Batuk",('Ya','Tidak'))
+
     col3,col4 = st.columns(2)
     with col3:
-        bp = st.number_input("Tekanan Darah",min_value=0,max_value=1000)
+        spek = st.number_input("Kualitas Oksigen Anda",min_value=0,max_value=1000)
     with col4:
-        chol = st.number_input("Kadar Kolesterol",min_value=0,max_value=1000)
+        nafas = st.number_input("Jumlah Nafas Dalam 1 Menit",min_value=0,max_value=1000)
+    col5,col6 = st.columns(2)
+    with col5:
+         nadi = st.number_input("Denyut Nadi Dalam 1 Menit",min_value=0,max_value=100)
+    with col6:
+         berat = st.number_input("Berat Badan (kg)",min_value=0,max_value=100)
+    
+    col11,col12 = st.columns(2)
+    with col11:
+        sesak = st.selectbox("Sesak",('Ya','Tidak'))
+    with col12:
+        parukesan = st.selectbox("Suara Nafas Paru",('Mengi/Bising Gesek','Normal'))
+    col7,col8= st.columns(2)
+    with col7:
+         sistol = st.number_input("Tekanan Sistolik (mmHG)",min_value=0,max_value=1000)
+    with col8:
+         diastol = st.number_input("Tekanan Diastolik (mmHG)",min_value=0,max_value=1000)
     #    Centering Butoon 
     columns = st.columns((2, 0.6, 2))
     sumbit = columns[1].button("Submit")
     if sumbit == True:
 
-        if  nama != '' and jk != '' and bp != 0 and chol != 0 and umur != 0:
-            if model_1 or model_2 or model_3:
-                if preprosesing == 'Normalization (Min-Max)':
+        if  nama != '' and jk != '' and spek != 0 and nafas != 0 and umur != 0:
                     # cek jenis kelamin
-                    #1 = laki-laki
-                    #0 = perempuan
+                    #0 = laki-laki
+                    #1 = perempuan
                     if jk == 'Laki-laki':
                         jk = 0
                     else:
                         jk = 1
-                    # normalisasi data
-                    data = metode.normalisasi([umur,jk,bp,chol])
-                    # prediksi data
-                    if model_1:
-                        prediksi = metode.knn(data)
-                        # cek prediksi
-                        with st.spinner("Tunggu Sebentar Masih Proses..."):
-                            if prediksi[-1] == 0:
-                                time.sleep(1)
-                                st.success("Hasil Prediksi Metode KNN: "+nama+" Tidak Ada Penyakit Jantung")
-                            else:
-                                time.sleep(1)
-                                st.warning("Hasil Prediksi Metode KNN: "+nama+" Ada Penyakit Jantung")
-                    if model_2:
-                        prediksi = metode.bagging(data)
-                        # cek prediksi
-                        with st.spinner("Tunggu Sebentar Masih Proses..."):
-                            if prediksi[-1] == 0:
-                                time.sleep(1)
-                                st.success("Hasil Prediksi Metode Bagging: "+nama+" Tidak Ada Penyakit Jantung")
-                            else:
-                                time.sleep(1)
-                                st.warning("Hasil Prediksi Metode Bagging: "+nama+" Ada Penyakit Jantung")
-                    if model_3:
-                        prediksi = metode.randomforest(data)
-                        # cek prediksi
-                        with st.spinner("Tunggu Sebentar Masih Proses..."):
-                            if prediksi[-1] == 0:
-                                time.sleep(1)
-                                st.success("Hasil Prediksi Metode Random Forest: "+nama+" Tidak Ada Penyakit Jantung")
-                            else:
-                                time.sleep(1)
-                                st.warning("Hasil Prediksi Metode Random Forest: "+nama+" Ada Penyakit Jantung")
-                else:
-                    # cek jenis kelamin
-                    #1 = laki-laki
-                    #0 = perempuan
-                    if jk == 'Laki-laki':
-                        jk = 1
+                    # tensi 
+                    tensi = metode.def_tensi(umur,sistol,diastol)
+                    if(tensi == 'Rendah'):
+                        tensi = 2
+                    elif(tensi == 'Normal'):
+                        tensi = 0
                     else:
-                        jk = 0
-                    # normalisasi data
-                    data = metode.normal([umur,jk,bp,chol])
-                    # prediksi data
-                    if model_1:
-                        prediksi = metode.knn_no_norm(data)
-                        # cek prediksi
-                        with st.spinner("Tunggu Sebentar Masih Proses..."):
-                            if prediksi[-1] == 0:
-                                time.sleep(1)
-                                st.success("Hasil Prediksi Metode KNN: "+nama+" Tidak Ada Penyakit Jantung")
-                            else:
-                                time.sleep(1)
-                                st.warning("Hasil Prediksi Metode KNN: "+nama+" Ada Penyakit Jantung")
-                    if model_2:
-                        prediksi = metode.bagging_no_norm(data)
-                        # cek prediksi
-                        with st.spinner("Tunggu Sebentar Masih Proses..."):
-                            if prediksi[-1] == 0:
-                                time.sleep(1)
-                                st.success("Hasil Prediksi Metode Bagging: "+nama+" Tidak Ada Penyakit Jantung")
-                            else:
-                                time.sleep(1)
-                                st.warning("Hasil Prediksi Metode Bagging: "+nama+" Ada Penyakit Jantung")
-                    if model_3:
-                        prediksi = metode.randomforest_no_norm(data)
-                        # cek prediksi
-                        with st.spinner("Tunggu Sebentar Masih Proses..."):
-                            if prediksi[-1] == 0:
-                                time.sleep(1)
-                                st.success("Hasil Prediksi Metode Random Forest: "+nama+" Tidak Ada Penyakit Jantung")
-                            else:
-                                time.sleep(1)
-                                st.warning("Hasil Prediksi Metode Random Forest: "+nama+" Ada Penyakit Jantung")
-            else:
-                st.error("Pilih Salah Satu Metode")
+                        tensi = 1
+                    # batuk
+                    if batuk == 'Ya':
+                        batuk = 1
+                    else:
+                        batuk = 0
+                    # sesak
+                    if sesak == 'Ya':
+                        sesak = 1
+                    else:
+                        sesak = 0
+                    # paru kesan
+                    if parukesan == 'Mengi/Bising Gesek':
+                        parukesan = 1
+                    else:
+                        parukesan = 0
+                    # normalisasi 
+                    # ,UMUR,JENKEL,BATUK,SESAK,SPESIFIKASI,PARU-KESAN,NAFAS,NADI,BB,TENSI
+                    normalisasi = metode.normalisasi([ umur, jk, batuk, sesak, spek, parukesan, nafas, nadi, berat, tensi])
+                    # klasifikasi
+                    klasifikasi = metode.svm(normalisasi)
+                    # cek klasifikasi
+                    with st.spinner("Tunggu Sebentar Masih Proses..."):
+                        if klasifikasi[-1] == 0:
+                            time.sleep(1)
+                            st.success("Hasil klasifikasi : **"+nama+"** Tidak Ada Paru-paru")
+                            st.balloons()
+                        else:
+                            time.sleep(1)
+                            st.warning("Hasil klasifikasi : **"+nama+"** Ada Paru-paru")        
+                            st.write("Silahkan Untuk Periksa Lebih Lanjut Ke Dokter" )
         else:
             st.error("Harap Diisi Semua Kolom")
 
 # about page
 if about==True and home==False:
-    url = 'https://www.kaggle.com/datasets/shrutipandit707/heartdisease'
-    n8 = 'https://lala09-erha.github.io/datamining/notebooks8.html'
-    n9 = 'https://lala09-erha.github.io/datamining/notebooks10.html'
-    s8 = 'https://lala09-erha.github.io/datamining/Salinan8.html'
-    s10 = 'https://lala09-erha.github.io/datamining/Salinan10.html'
     st.markdown("<h2 style='text-align: center; color: white; margin:0 ; padding:0;'>Tentang Sistem ini</h2>", unsafe_allow_html=True)
-    st.write('Sistem Prediksi Penyakit Jantung adalah sebuah sistem yang bertujuan untuk memprediksi penyakit jantung dini. Sistem ini dibuat menggunakan bahasa pemrograman python dan library streamlit.')
-    st.markdown("<h4 style='text-align: center; color: white; margin:0 ; padding:0;'>Dataset</h4>", unsafe_allow_html=True)
-    st.markdown("<p  color: white;'>Dataset yang digunakan pada sistem ini memiliki <b>5 fitur</b> termasuk kelas, Dataset yang digunakan dalam sistem ini menggunakan data yang didapatkan melalui repository dataset yang berada pada website Kaggle.com . Dataset yang berjudul <i>Heart Disease</i>, dataset untuk mendeteksi apakah seseorang mengidap Penyakit Jantung atau tidak berdasarkan berbagai faktor seperti <i>cholestrol</i> (mg/dL),<i>blood pressure</i> (mm Hg),<i>sex</i> (female/male),<i>age</i>,class (0/1), semua fitur yang disebutkan bertipe numerik.</p>", unsafe_allow_html=True)
-    st.write('Untuk Fitur kolestrol bisa didapatkan dengan cara mengecek ke dokter atau dengan cara mengukur sendiri dengan menggunakan alat yang disediakan oleh dokter. Untuk fitur blood pressure bisa didapatkan dengan cara mengecek ke dokter atau dengan cara mengukur sendiri dengan menggunakan alat yang disediakan oleh dokter.')
-    st.info("Dataset : [link](%s)" % url,icon="ℹ️")
-    st.markdown("<h4 style='text-align: center; color: white; margin:0 ; padding:0;'>Tahap preprosessing</h4>", unsafe_allow_html=True)
-    st.write('Sistem ini menggunakan preprosessing data yaitu dengan normalisasi data. Metode normalisasi yang digunakan adalah MinMaxScaler.')
-    st.markdown("<h4 style='text-align: center; color: white; margin:0 ; padding:0;'>Metode yang digunakan</h4>", unsafe_allow_html=True)
-    st.markdown("<p  color: white;'>Pada sistem ini menggunakan model KNN ( <i>K-nearest neighbors algorithm</i> ) dengan parameter <b>K = 11</b> dengan akurasi 75% jika sebelumnya data dinormalisasi terlebih dahulu, namun jika tanpa normalisasi akurasi yang didapat sebesar 70% .</p>", unsafe_allow_html=True)
-    st.write('Selanjutnya memakai model Bagging Clasfifier dengan base_estimator = gaussianNB dan n_estimators = 6 dengan akurasi sebesar 77% jika sebelumnya data dinormalisasi terlebih dahulu, namun jika tanpa tanpa normalisasi data menggunakan n_estimators = 2 dengan akurasi sebesar 77%.')
-    st.write('Terakhir memakai model Random Forest dengan n_estimators = 13 dengan akurasi sebesar 72% jika sebelumnya data dinormalisasi terlebih dahulu, namun jika tanpa tanpa normalisasi data menggunakan n_estimators = 14 dengan akurasi sebesar 72% juga.')
-    st.markdown("<h4 style='text-align: center; color: white; margin:0 ; padding:0;'>Penjelasan Singkat</h4>", unsafe_allow_html=True)
-    st.write('Ketiga metode tersebut ialah metode klasifikasi,alasan menggunakan ketiga model tersebut adalah karena memiliki akurasi yang terbesar dari model lainnya pada dataset ini, sehingga diputuskan untuk menggunakan model tersebut. Sebelumnya sudah dilakukan empat analisa dan percobaan untuk model lainnya dengan kondisi data yang tanpa menggunakan maupun menggunakan normalisasi sebelumnya, lebih lengkapnya pada link dibawah.')
-    st.info("[Percobaan model pertama](%s) | [Percobaan model Kedua](%s) | [Percobaan model Ketiga](%s) | [Percobaan model Keempat](%s)" % (n8,n9,s8,s10),icon="ℹ️")        
+    st.write('Sistem ini dibuat untuk mempermudah dalam proses diagnosa **dini** Penyakit paru-paru, menggunakan metode **SVM**')
+    st.markdown("<h4 style='text-align: center; color: white; margin:0 ; padding:0;'>Tentang Paramater Pemeriksaaan dan Cara Mengetahui</h4>", unsafe_allow_html=True)
+    st.write('**Tekanan Kualitas Oksigen** adalah Kualitas Oksigen yang berada pada tubuh anda. Cara mengetahuinya menggunakan Alat Pantau Oksigen dalam Tubuh / **Oximeter**')
+    st.write('**Denyut Nadi** adalah Denyut Nadi yang berada pada tubuh anda. Cara mengetahuinya cukup dengan memegang bagian leher anda dan hitung berapa kali anda merasa dalam 1 menit')
+    st.write('**Suara Nafas Paru** adalah Suara Nafas yang berada pada tubuh anda. Cara mengetahuinya dengan menarik nafas dalam-dalam dan hembuskan nafas')
+    st.write('**Tekanan Sistolik** adalah tekanan darah pada saat jantung memompa darah atau saat berkontraksi, sedangkan **Diastolik** adalah tekanan darah pada saat jantung relaksasi. Cara mengetahuinya dengan menggunakan alat **tensi menter**')
--- metode.py
+++ metode.py
@@ -4,46 +4,53 @@ import pandas as pd
 # preprocessing
 def normalisasi(x):
     # import data test
-    cols = ['age','sex','BP','cholestrol']
-    df = pd.DataFrame([x],columns=cols)
-    data_test = pd.read_csv('model/data_test2.csv')
-    data_test = data_test.drop(data_test.columns[0],axis=1)
+    data_test = pd.read_csv('data/testingdata.csv')
+    data_test = data_test.drop(data_test.columns[0], axis=1)
     # memasukkan data kedalam data test
-    data_test = data_test.append(other=df,ignore_index=True)
-    # return data_test yang sudah dinormalisasi
-    return joblib.load('model/norm.sav').fit_transform(data_test)
-
-# normal
-def normal(x):
-    cols = ['age','sex','BP','cholestrol']
-    df = pd.DataFrame([x],columns=cols)
-    data_test = pd.read_csv('model/data_test2.csv')
-    data_test = data_test.drop(data_test.columns[0],axis=1)
-    # memasukkan data kedalam data test
-    data_test = data_test.append(other=df,ignore_index=True)
-    # return data_test yang sudah dinormalisasi
-    return (data_test)
-
-# metode with normalization
-def knn(x):
+    cols = data_test.columns
+    df = pd.DataFrame([x], columns=cols)
+    data_test = data_test.append(other=df, ignore_index=True)
     
-    return joblib.load('model/modelKNN11.pkl').predict(x)
+    for col in cols:
+        data_test[col] = data_test[col] / data_test[col].max()
+    return data_test
 
-def bagging(x):
-    return joblib.load('model/bagginggaussian.pkl').predict(x)
+def svm(x):
+    return joblib.load('model/modelSVMRISET.pkl').predict(x)
 
-def randomforest(x):
-    return joblib.load('model/randomforest.pkl').predict(x)
-
-# metode without normalization
-def knn_no_norm(x):
-    return joblib.load('model/modelKNN11_1.pkl').predict(x)
-
-def bagging_no_norm(x):
-    return joblib.load('model/bagginggaussian_1.pkl').predict(x)
-
-def randomforest_no_norm(x):
-    return joblib.load('model/randomforest_1.pkl').predict(x)
-
-
-# print(normalisasi([50,1,120,200]))
+def def_tensi(umur,sistol,dias):
+  if umur >= 14 and umur <= 18 :
+    if(sistol < 90 and dias <50):
+      return "Rendah"
+    elif (90 <= sistol <= 120) and (50 <= dias <=80):
+      return "Normal"
+    else:
+      return "Tinggi"
+  elif umur >= 19 and umur <= 40:
+    if(sistol < 95 and dias <60):
+      return "Rendah"
+    elif (95 <= sistol <= 135) and (60 <= dias <=80):
+      return "Normal"
+    else:
+      return "Tinggi"
+  elif umur >= 41 and umur <=60 :
+    if(sistol < 110 and dias < 70):
+      return "Rendah"
+    elif (110 <= sistol <= 145) and (70 <= dias <=90):
+      return "Normal"
+    else:
+      return "Tinggi"
+  elif umur > 60 :
+    if(sistol < 95 and dias < 70):
+      return "Rendah"
+    elif (95 <= sistol <= 145) and (70 <= dias <=90):
+      return "Normal"
+    else:
+      return "Tinggi"
+  else:
+    if(sistol < 80 and dias < 45):
+      return "Rendah"
+    elif (80 <= sistol <= 120) and (45 <= dias <=80):
+      return "Normal"
+    else:
+      return "Tinggi"
